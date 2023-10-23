a0db6eb62bc512d64b6543d4f6d082293b08ab34
Brandon Lo <lobrandon667@gmail.com>
2023-10-23 15:42:14 -0400

    added scripts and prefabs to folders

---
+++ testproject/Assets/Scripts/DoorAnimation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour
+{
+    public bool isLocked = true;
+    public Animator door = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && other.GetComponent<PlayerInventory>().hasKey) 
+        {
+            UnlockDoor();
+        }
+    }
+
+    private void UnlockDoor()
+    {
+        if (isLocked)
+        {
+            isLocked = false;
+            door.SetTrigger("DoorOpen");
+        }
+    }
+
+}
