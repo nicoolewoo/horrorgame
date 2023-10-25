33ccec3fc9547bde08cc2f5f1f071c7636a7cd94
Evan Ding <evanding@Evans-MacBook-Air.local>
2023-10-24 22:27:54 -0400

    Door stuff

---
+++ testproject/Assets/Scripts/DoorCellOpen.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorCellOpen : MonoBehaviour
+{
+    public float TheDistance;
+    public GameObject ActionDisplay;
+    public GameObject ActionText;
+    public GameObject TheDoor;
+	void Update()
+	{
+		TheDistance = PlayerCasting.DistanceFromTarget;
+	}
+
+	void OnMouseOver()
+	{
+		if (TheDistance <= 8)
+		{
+			ActionDisplay.SetActive(true);
+			ActionText.SetActive(true);
+		}
+		if (Input.GetButtonDown("Action"))
+		{
+			if (TheDistance <= 2)
+			{
+				this.GetComponent<BoxCollider>().enabled = false;
+				ActionDisplay.SetActive(false);
+				ActionText.SetActive(false);
+				TheDoor.GetComponent<Animation>().Play("FirstDoorOpenAnim");
+				
+			}
+		}
+	}
+
+	void OnMouseExit()
+	{
+		ActionDisplay.SetActive(false);
+		ActionText.SetActive(false);
+	}
+
+}
