20e5c52a129cca8fc8adb66cffd6b6b25b516348
Brandon Lo <lobrandon667@gmail.com>
2023-10-26 23:38:26 -0400

    added key collectibles and player inventory

--- testproject/Assets/Scripts/Collectibles/PlayerInventory.cs
+++ testproject/Assets/Scripts/Collectibles/PlayerInventory.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     public bool hasKey = false;
-
+    public int keyCount = 0;
     public void AddKey()
     {
-        hasKey = true;
+        keyCount++;
+        Debug.Log("Key added! Total keys: " + keyCount);
+        if (keyCount == 5)
+        {
+            hasKey = true;
+        }
+        
     }
 }
